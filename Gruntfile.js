a6565ac719d09e1423aa7bda8ed76b9666a714a4
jorgematias87 <jorgematias87@gmail.com>
2015-06-06 00:15:08 +0000

    livereload for Cloud9 -  Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,10 +65,15 @@ module.exports = function (grunt) {
 
     // The actual grunt server settings
     connect: {
+      // options: {
+      //   port: 9000,
+      //   // Change this to '0.0.0.0' to access the server from outside.
+      //   hostname: 'localhost',
+      //   livereload: 35729
+      // },
       options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        port: process.env.PORT,
+        hostname: process.env.IP,
         livereload: 35729
       },
       livereload: {
